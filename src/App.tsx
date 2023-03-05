8f93430b302bb80c3a7534fe1e1889bcd387cb26
Yuriy Shchebetun <yuriyshchebetun@gmail.com>
2023-03-05 16:39:29 +0200

    add routing and splitting-lazy

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
-import { Counter } from "./components/Counter"
+import { Suspense } from "react"
+import { Link } from "react-router-dom"
+import { Route, Routes } from "react-router-dom"
 import "./index.scss"
+import { AboutPageLazy } from "./pages/AboutPage/AboutPage.lazy"
+import { MainPageLazy } from "./pages/MainPage/MainPage.lazy"
 
 const App = () => {
   return (
     <div className="app">
-      Hello form App component
-      <Counter />
+      <Link to="/">Home page</Link>
+      <Link to="/about">About page</Link>
+      <Suspense fallback="Loading...">
+        <Routes>
+          <Route path="/" element={<MainPageLazy />} />
+          <Route path="/about" element={<AboutPageLazy />} />
+        </Routes>
+      </Suspense>
     </div>
   )
 }
