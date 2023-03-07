bd6f14cbdf7c38ce090794bbda3e1cf58bc7f8c5
Yuriy Shchebetun <yuriyshchebetun@gmail.com>
2023-03-07 18:50:46 +0200

    add classNames helper

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import "./styles/index.scss"
 import { AboutPageLazy } from "./pages/AboutPage/AboutPage.lazy"
 import { MainPageLazy } from "./pages/MainPage/MainPage.lazy"
 import { useTheme } from "./theme/useTheme"
+import { classNames } from "./helpers/classNames/classNames"
 
 
 const App = () => {
@@ -12,7 +13,7 @@ const App = () => {
   const { theme, toggleTheme } = useTheme();
 
   return (
-    <div className={`app ${theme}`}>
+    <div className={classNames('app', {}, [theme])}>
       <button onClick={() => toggleTheme()}>Toggle theme</button>
       <Link to="/">Home page</Link>
       <Link to="/about">About page</Link>
---
+++ src/helpers/classNames/classNames.ts
@@ -0,0 +1,17 @@
+type Mods = Record<string, boolean | string>;
+
+export function classNames(
+  cls: string,
+  mods: Mods,
+  additional: string[]
+): string {
+  return [
+    cls,
+    ...additional,
+    ...Object.entries(mods)
+      .filter(([className, value]) => Boolean(value))
+      .map(([className]) => className),
+  ].join(" ");
+}
+
+// classNames("button", { "button--primary": true, "button--disabled": false }, ["button--small", "button--large"])
