c670d08ef7165f5acd99d71754fe3f1a4d33a9ff
Yuriy Shchebetun <yuriyshchebetun@gmail.com>
2023-03-07 14:30:17 +0200

    add functionality to change themes

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
 import { Suspense } from "react"
 import { Link } from "react-router-dom"
 import { Route, Routes } from "react-router-dom"
-import "./index.scss"
+import "./styles/index.scss"
 import { AboutPageLazy } from "./pages/AboutPage/AboutPage.lazy"
 import { MainPageLazy } from "./pages/MainPage/MainPage.lazy"
+import { useTheme } from "./theme/useTheme"
+
 
 const App = () => {
+
+  const { theme, toggleTheme } = useTheme();
+
   return (
-    <div className="app">
+    <div className={`app ${theme}`}>
+      <button onClick={() => toggleTheme()}>Toggle theme</button>
       <Link to="/">Home page</Link>
       <Link to="/about">About page</Link>
       <Suspense fallback="Loading...">
---
+++ src/theme/ThemeProvider.tsx
@@ -0,0 +1,22 @@
+import { FC, useMemo, useState } from "react";
+import { Theme, ThemeContext } from "./ThemeContext";
+
+const defaultTheme = localStorage.getItem("theme") as Theme || Theme.LIGHT;
+
+const ThemeProvider: FC = ({ children }) => {
+
+  const [theme, setTheme] = useState<Theme>(defaultTheme);
+
+  const defaultProps = useMemo(() => ({
+    theme,
+    setTheme
+  }), [theme])
+
+  return (
+    <ThemeContext.Provider value={defaultProps}>
+      {children}
+    </ThemeContext.Provider>
+  )
+}
+
+export default ThemeProvider
