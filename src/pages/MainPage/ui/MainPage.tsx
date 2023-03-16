42ce8d6b451bb8a242ef90ad9ec4a22ad5c26c0b
Yuriy Shchebetun <yuriyshchebetun@gmail.com>
2023-03-16 14:26:22 +0200

    remove test components from main page

--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -1,5 +1,3 @@
-import { BugButton } from 'app/providers/ErrorBoundary';
-import { Counter } from 'entities/Counter';
 import { useTranslation } from 'react-i18next';
 
 function MainPage() {
@@ -8,8 +6,6 @@ function MainPage() {
   return (
     <div>
       {t('main-page')}
-      <BugButton />
-      <Counter />
     </div>
   );
 }
