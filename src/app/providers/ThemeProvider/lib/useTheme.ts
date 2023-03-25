35c8dbfaf90f20ba61ac42bae98b2beb0816013f
Yuriy Shchebetun <yuriyshchebetun@gmail.com>
2023-03-25 15:43:31 +0200

    add orange theme

--- src/app/providers/ThemeProvider/lib/useTheme.ts
+++ src/app/providers/ThemeProvider/lib/useTheme.ts
@@ -13,6 +13,22 @@ export function useTheme(): IUseThemeResult {
     const newTheme = theme === Theme.LIGHT ? Theme.DARK : Theme.LIGHT;
     setTheme?.(newTheme);
     localStorage.setItem(LOCAL_STORAGE_THEME_KEY, newTheme);
+    // let newTheme: Theme;
+    // switch (theme) {
+    // case Theme.DARK:
+    //   newTheme = Theme.LIGHT;
+    //   break;
+    // case Theme.LIGHT:
+    //   newTheme = Theme.ORANGE;
+    //   break;
+    // case Theme.ORANGE:
+    //   newTheme = Theme.DARK;
+    //   break;
+    // default:
+    //   newTheme = Theme.LIGHT;
+    // }
+    // setTheme?.(newTheme);
+    // localStorage.setItem(LOCAL_STORAGE_THEME_KEY, newTheme);
   };
 
   return {
