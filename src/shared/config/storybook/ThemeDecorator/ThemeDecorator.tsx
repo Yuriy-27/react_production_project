fa004c73d9db2a044ace54db5faa6f95d11926ea
Yuriy Shchebetun <yuriyshchebetun@gmail.com>
2023-05-31 14:31:44 +0300

    fix storybook config

--- src/shared/config/storybook/ThemeDecorator/ThemeDecorator.tsx
+++ src/shared/config/storybook/ThemeDecorator/ThemeDecorator.tsx
@@ -1,6 +1,7 @@
 import { Story } from '@storybook/react';
-import { Theme, ThemeProvider } from '@/app/providers/ThemeProvider';
 import '@/app/styles/index.scss';
+import { Theme } from '@/shared/constants/theme';
+import { ThemeProvider } from '@/app/providers/ThemeProvider';
 
 export const ThemeDecorator = (theme: Theme) => (StoryComponent: Story) => (
   <ThemeProvider initialTheme={theme}>
