e547e1bdbdf76bcb9662130788df1bd299902174
Yuriy Shchebetun <yuriyshchebetun@gmail.com>
2023-05-31 12:19:38 +0300

    fix storybook

---
+++ src/entities/Notification/ui/NotificationList/NotificationList.stories.tsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+import withMock from 'storybook-addon-mock';
+import { StoreDecorator } from '@/shared/config/storybook/StoreDecorator/StoreDecorator';
+import { NotificationList } from './NotificationList';
+
+export default {
+  title: 'entities/Notification/NotificationList',
+  component: NotificationList,
+  argTypes: {
+    backgroundColor: { control: 'color' },
+  },
+  decorators: [withMock],
+} as ComponentMeta<typeof NotificationList>;
+
+const Template: ComponentStory<typeof NotificationList> = (args) => <NotificationList {...args} />;
+
+export const Normal = Template.bind({});
+Normal.args = {};
+Normal.decorators = [StoreDecorator({})];
+Normal.parameters = {
+  mockData: [
+    {
+      url: `${__API__}notifications`,
+      method: 'GET',
+      status: 200,
+      response: [
+        {
+          id: '1',
+          title: 'Notification 1',
+          description: 'Notification 1 content',
+        },
+        {
+          id: '2',
+          title: 'Notification 2',
+          description: 'Notification 2 content',
+        },
+        {
+          id: '3',
+          title: 'Notification 3',
+          description: 'Notification 3 content',
+        },
+      ],
+    },
+  ],
+};
